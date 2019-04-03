bb63cc8006aa8e355151cca475c612f692ff6ab0
saylorsa <saylorsa@rose-hulman.edu>
2019-04-03 11:14:42 -0400

    practice_problem1, Done - 6

--- src/practice_problem1.py
+++ src/practice_problem1.py
@@ -43,8 +43,8 @@ def main():
     # run_test_init()
     # run_test_append_string()
     # run_test_double()
-    run_test_shrink()
-    # run_test_double_then_shrink()
+    # run_test_shrink()
+    run_test_double_then_shrink()
     # run_test_reset()
     # run_test_steal()
     # run_test_get_history()
@@ -319,7 +319,7 @@ class Box(object):
           :type new_volume: int
         """
         # ---------------------------------------------------------------------
-        # TODO: 6. Implement and test this function.
+        # DONE: 6. Implement and test this function.
         #     The testing code is already written for you (above).
         # ---------------------------------------------------------------------
         # ---------------------------------------------------------------------
@@ -327,6 +327,9 @@ class Box(object):
         #    DIFFICULTY:      5
         #    TIME ESTIMATE:   5 minutes.
         # ---------------------------------------------------------------------
+        m = self.double()
+        n = self.shrink(new_volume)
+        return len(m + n)
 
     def reset(self):
         """
