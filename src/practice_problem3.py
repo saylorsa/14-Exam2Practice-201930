28a0a8277ca427701c8b99fe4bc9f0c60c8a41c0
saylorsa <saylorsa@rose-hulman.edu>
2019-04-17 19:25:50 -0400

    practice_problem3, Done - 4

--- src/practice_problem3.py
+++ src/practice_problem3.py
@@ -481,13 +481,18 @@ def practice_problem3c(sequence):
       :type: sequence: list    or tuple or string
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   8 minutes.
     ###########################################################################
+    index = []
+    for k in range(len(sequence)):
+        if sequence[k] == 0:
+            index = index + [k]
+    return index
 
 
 def run_test_practice_problem3d():
